35b81aafa9455e522c48e93406790ba1e78c3edf
Mathijs Molenaar <mathijs.l.molenaar@gmail.com>
2014-06-16 16:51:07 +0200

    Change FilterArrayAdapter to an FilterListAdapter that uses Lists and extends from a BaseAdapter

--- app/src/main/java/zwaggerboyz/instaswaggify/MainActivity.java
+++ app/src/main/java/zwaggerboyz/instaswaggify/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends Activity {
     ListView mListView;
@@ -15,12 +17,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListItemFilter[] items = new ListItemFilter[20];
+        ArrayList<ListItemFilter> items = new ArrayList<ListItemFilter>();
         for (int i = 0; i < 20; i++) {
-            items[i] = new ListItemFilter();
+            items.add(new ListItemFilter());
         }
 
-        FilterArrayAdapter adapter = new FilterArrayAdapter(this, items);
+        FilterListAdapter adapter = new FilterListAdapter(this, items);
         mListView = (ListView)findViewById(R.id.activity_main_listview);
         mListView.setAdapter(adapter);
     }
