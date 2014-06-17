a921ca1059678eb89851cd6c9c22e70c62c4d302
Mathijs Molenaar <mathijs.l.molenaar@gmail.com>
2014-06-17 11:59:17 +0200

    Add renderscript (support mode) and create Interface for filters

---
+++ app/src/main/java/zwaggerboyz/instaswaggify/IFilter.java
@@ -0,0 +1,22 @@
+package zwaggerboyz.instaswaggify;
+
+
+import android.support.v8.renderscript.RenderScript;
+import android.support.v8.renderscript.Script;
+import android.support.v8.renderscript.ScriptGroup;
+
+/**
+ * Created by Mathijs on 17/06/14.
+ */
+public interface IFilter {
+    public String getName();
+    public String getLabel(int i);
+    public String setLabel(int i);
+    public int getValue(int i);
+    public void setValue(int i, int value);
+    public void setRS(RenderScript rs);
+    public int getNumValues();
+    public void addKernel(ScriptGroup.Builder scriptBuilder);
+    public Script.KernelID getKernelId();
+    public Script.FieldID getFieldId();
+}
