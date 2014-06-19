f997c89cf113c21bf849885067f0c8df03eb5cce
Peter Verkade <peter.m.verkade@gmail.com>
2014-06-19 16:51:53 +0200

    touchen

--- app/src/main/java/zwaggerboyz/instaswaggify/CanvasDraggableItem.java
+++ app/src/main/java/zwaggerboyz/instaswaggify/CanvasDraggableItem.java
@@ -1,69 +1,56 @@
 package zwaggerboyz.instaswaggify;
 
 import android.graphics.Bitmap;
+import android.graphics.Rect;
 
 /**
  * Created by Peter on 19-6-2014.
  */
 public class CanvasDraggableItem {
-    private float x, y;
-    private int width, height;
-    private boolean selected = false;
+    private float left, top, right, bottom;
     private Bitmap bitmap;
 
-    public CanvasDraggableItem (Bitmap bitmap, float x, float y) {
+    public CanvasDraggableItem (Bitmap bitmap, float left, float top, float right, float bottom) {
         this.bitmap = bitmap;
-        this.x = x;
-        this.y = y;
-        this.width = bitmap.getWidth();
-        this.height = bitmap.getHeight();
+        this.left = left;
+        this.right = right;
+        this.top = top;
+        this.bottom = bottom;
+    }
+
+    public void move (float newX, float newY) {
+        if (newX >= 0 && newX <= 1 && newY >= 0 && newY <= 1) {
+            float width = right - left;
+            float height = top - bottom;
+
+            left = newX - 0.5f * width;
+            right = newX + 0.5f * width;
+            top = newY - 0.5F * height;
+            bottom = newY + 0.5f * height;
+        }
+    }
+
+    public float getLeft() {
+        return left;
+    }
+
+    public float getTop() {
+        return top;
+    }
+
+    public float getRight() {
+        return right;
+    }
+
+    public float getBottom() {
+        return bottom;
+    }
+
+    public boolean isWithinBounds (float x, float y) {
+        return (x >= left && x <= right && y >= top && y <= bottom);
     }
 
     public Bitmap getBitmap() {
         return bitmap;
-    }
-
-    public void setBitmap(Bitmap bitmap) {
-        this.bitmap = bitmap;
-    }
-
-    public float getX() {
-        return x;
-    }
-
-    public void setX(float x) {
-        this.x = x;
-    }
-
-    public float getY() {
-        return y;
-    }
-
-    public void setY(float y) {
-        this.y = y;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public boolean isSelected() {
-        return selected;
-    }
-
-    public void setSelected(boolean selected) {
-        this.selected = selected;
     }
 }
